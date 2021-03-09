89a647bec1c898bb68a5d5e3a5cea3ade7a8ebf9
Jimin L <69053105+chocolee13@users.noreply.github.com>
2021-03-09 22:51:58 +0800

    sumOfRoots

--- sumofroots.java
+++ sumofroots.java
@@ -1,9 +1,7 @@
 /*
-
 Original question:
 Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in
 this way until a single-digit number is produced. The input will be a non-negative integer.
-
 */
 
 public class DRoot {
