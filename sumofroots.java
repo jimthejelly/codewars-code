eeee160a8eaaa1e2af37eff618213d4a4a7e1dc6
Jimin L <69053105+chocolee13@users.noreply.github.com>
2021-03-09 22:09:49 +0800

    Create sumofroots.java
    Added question and code

---
+++ sumofroots.java
@@ -0,0 +1,23 @@
+/*
+Original question:
+Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in
+this way until a single-digit number is produced. The input will be a non-negative integer.
+*/
+
+public class DRoot {
+  public static int digital_root(int n) {
+    String fn = n + "";
+    int num = 0;
+    
+    while(fn.length()!=1) {
+      num=0;
+      for(int i=0; i<fn.length(); i++) {
+        String sn = fn.charAt(i) + "";
+        num += Integer.parseInt(sn);
+      }
+      fn = num + "";
+    }
+    
+    return num;
+  }
+}
