3282d87b1b5093847d447b5efc18657e6ece312c
Jimin L <69053105+chocothelee@users.noreply.github.com>
2021-03-31 20:59:34 +0800

    phoneNumber

---
+++ phonenumber.java
@@ -0,0 +1,28 @@
+/*
+Question:
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+The returned format must be correct in order to complete this challenge. Don't forget the space after the closing parentheses!
+*/
+
+public class Kata {
+  public static String createPhoneNumber(int[] numbers) {
+    String pnum = "(";
+    
+    for(int i=0; i<3; i++) {
+      pnum += numbers[i] + "";
+    }
+    pnum += ") ";
+    
+    for(int j=3; j<6; j++) {
+      pnum += numbers[j] + "";
+    }
+    pnum += "-";
+    
+    for(int p=6; p<10; p++) {
+      pnum+= numbers[p] + "";
+    }
+    
+    return pnum;
+  }
+}
